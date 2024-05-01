8550343897ca76b02f2a62e06373c0f454bea1a6
Greg <gregorycarvalho3@gmail.com>
2024-05-01 10:37:35 +0200

    Changement de genre_afficher.html

--- APP_FILMS_164/genres/gestion_genres_wtf_forms.py
+++ APP_FILMS_164/genres/gestion_genres_wtf_forms.py
@@ -16,14 +16,22 @@ class FormWTFAjouterGenres(FlaskForm):
         Définition d'un "bouton" submit avec un libellé personnalisé.
     """
     nom_genre_regexp = "^([A-Z]|[a-zÀ-ÖØ-öø-ÿ])[A-Za-zÀ-ÖØ-öø-ÿ]*['\- ]?[A-Za-zÀ-ÖØ-öø-ÿ]+$"
-    nom_genre_wtf = StringField("Clavioter le genre ", validators=[Length(min=2, max=20, message="min 2 max 20"),
+    nom_genre_wtf = StringField("Insérer le nom du client ", validators=[Length(min=2, max=20, message="min 2 max 20"),
                                                                    Regexp(nom_genre_regexp,
                                                                           message="Pas de chiffres, de caractères "
                                                                                   "spéciaux, "
                                                                                   "d'espace à double, de double "
                                                                                   "apostrophe, de double trait union")
                                                                    ])
-    submit = SubmitField("Enregistrer genre")
+    prenom_genre_regexp = "^([A-Z]|[a-zÀ-ÖØ-öø-ÿ])[A-Za-zÀ-ÖØ-öø-ÿ]*['\- ]?[A-Za-zÀ-ÖØ-öø-ÿ]+$"
+    prenom_genre_wtf = StringField("Insérer le prénom du client ", validators=[Length(min=2, max=20, message="min 2 max 20"),
+                                                                         Regexp(nom_genre_regexp,
+                                                                                message="Pas de chiffres, de caractères "
+                                                                                        "spéciaux, "
+                                                                                        "d'espace à double, de double "
+                                                                                        "apostrophe, de double trait union")
+                                                                         ])
+    submit = SubmitField("Enregistrer Client")
 
 
 class FormWTFUpdateGenre(FlaskForm):
